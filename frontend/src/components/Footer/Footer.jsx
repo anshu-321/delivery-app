66c7e21b5d3d13d9d5972441fc3627fbdac762ad
anshu-321 <anshutoid@gmail.com>
2024-07-17 00:25:53 +0530

    Added responsiveness to all components

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>Loren Impsum random</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-right">
+          <h2>TOMATO</h2>
+          <ul>
+            <li>Home</li>
+            <li>About US</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-center">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+9192929292292</li>
+            <li>contact@anshu.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
